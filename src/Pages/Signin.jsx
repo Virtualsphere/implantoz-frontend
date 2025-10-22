a4fccf89ad2354b4320b3244e35bcd2e577b6be2
adityabobby2003 <aditya@virtualspheretechnologies.in>
2025-10-22 16:57:50 +0530

    Update Signin.jsx

--- src/Pages/Signin.jsx
+++ src/Pages/Signin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Logo from '../assets/logo.png';
+import Logo from '../assets/Logo.png';
 import Pic from '../assets/Pic.png'
 
 const Signin = () => {
